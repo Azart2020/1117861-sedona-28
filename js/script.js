e02d3020055859a717c950e1ae7bea27f58afa1c
Azart1 <55438913+Azart1@users.noreply.github.com>
2020-07-02 01:50:02 +0300

    добвил сетку на гридах

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
-
 let form = document.querySelector(".form-container");
 let buttonForm = document.querySelector(".button-open-form");
 let searchForm = document.querySelector(".search-form");
 let inputDate = document.querySelector(".input-date");
 let inputNumber = document.querySelector(".input-number");
 
-buttonForm.addEventListener("click", function (evt) {
+buttonForm.addEventListener("click", function(evt) {
   evt.preventDefault();
   form.classList.toggle('modal-hidden');
   form.classList.toggle('modal-open');
 });
 
 
-searchForm.addEventListener("submit", function (evt) {
-	if (!inputDate.value || !inputNumber.value) {
+searchForm.addEventListener("submit", function(evt) {
+  if (!inputDate.value || !inputNumber.value) {
     evt.preventDefault();
     console.log('Заполните все поля')
   }
 });
 
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
-      form.classList.toggle('modal-hidden');
-      form.classList.toggle('modal-open');
-    
+    form.classList.toggle('modal-hidden');
+    form.classList.toggle('modal-open');
+
   }
 });
