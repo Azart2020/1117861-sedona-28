bb5e41d512f8fc9873c28e892ef38678fb648132
Azart1 <55438913+Azart1@users.noreply.github.com>
2020-07-09 20:04:37 +0300

    правки
    смена формата изображений.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-let form = document.querySelector(".form-container");
-let buttonForm = document.querySelector(".button-open-form");
-let searchForm = document.querySelector(".search-form");
-let inputDate = document.querySelector(".input-date");
-let inputNumber = document.querySelector(".input-number");
+let form = document.querySelector('.form-container');
+let buttonForm = document.querySelector('.button-open-form');
+let searchForm = document.querySelector('.search-form');
+let inputDate = document.querySelector('.input-date');
+let inputNumber = document.querySelector('.input-number');
 
-buttonForm.addEventListener("click", function(evt) {
+  form.classList.toggle('modal-hidden');
+  form.classList.toggle('modal-open');
+  
+buttonForm.addEventListener('click', function(evt) {
   evt.preventDefault();
   form.classList.toggle('modal-hidden');
   form.classList.toggle('modal-open');
 });
 
 
-searchForm.addEventListener("submit", function(evt) {
+
+searchForm.addEventListener('submit', function(evt) {
   if (!inputDate.value || !inputNumber.value) {
     evt.preventDefault();
     console.log('Заполните все поля')
@@ -19,7 +23,7 @@ searchForm.addEventListener("submit", function(evt) {
 });
 
 
-window.addEventListener("keydown", function(evt) {
+window.addEventListener('keydown', function(evt) {
   if (evt.keyCode === 27) {
     form.classList.toggle('modal-hidden');
     form.classList.toggle('modal-open');
